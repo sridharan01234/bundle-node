13e4fbbb6baf5bc2d2c8165a145bcefd3d7aaeb0
sridhar.thillai <sridhar.thillai@aspiresys.com>
2025-06-02 16:45:29 +0530

    feat: Update build scripts to include compilation for vscode-extension and remove unused webview bundling

--- vscode-extension/esbuild.js
+++ vscode-extension/esbuild.js
@@ -1,9 +1,1 @@
-const esbuild = require("esbuild");
-const path = require("path");
-const fs = require("fs");
-
-// Since we're no longer using React, we can remove the webview build process
-// The webview is now pure HTML/CSS/JS embedded in the webviewUtils.ts file
-
-console.log("No webview bundling needed - using inline HTML/CSS/JS approach");
 console.log("Extension compilation handled by TypeScript compiler");
